4fed811d56ca2e1223c94bc13dfd3e18af741692
Caue Queiroz <cauenqueiroz@gmail.com>
2017-11-03 15:56:19 -0200

    Create smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,30 @@
 import { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchPlaylists, searchTracks } from '../src/main';
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
+
+  describe('Smoke tests', () => {
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+
+
 
 });
