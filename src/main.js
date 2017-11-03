b594013b27f924a13393ead53edae14ba2ac7a75
Caue Queiroz <cauenqueiroz@gmail.com>
2017-11-03 17:03:46 -0200

    Finish generic search test

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-export const search = () => {};
+export const search = (query, type) =>
+  fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+    .then(data => data.json());
+
 export const searchAlbums = () => {};
 export const searchArtists = () => {};
 export const searchTracks = () => {};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,13 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+
 import { search, searchAlbums, searchArtists, searchPlaylists, searchTracks } from '../src/main';
+
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+global.fetch = require('node-fetch');
 
 describe('Spotify Wrapper', () => {
 
@@ -25,6 +33,43 @@ describe('Spotify Wrapper', () => {
     });
   });
 
+  describe('Generic search', () => {
+    let fetchedStub;
+    let promise;
 
+    beforeEach(() => {
+      fetchedStub = sinon.stub(global, 'fetch');
+      promise = fetchedStub.returnsPromise();
+    });
+    afterEach(() => fetchedStub.restore());
+
+    it('should call fetch function', () => {
+      const artists = search();
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should call fetch with the correct url', () => {
+      context('passing just on type', () => {
+        const artists = search('blackbear', 'artist');
+        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=blackbear&type=artist');
+
+        const albums = search('blackbear', 'album');
+        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=blackbear&type=album');
+      });
+
+      context('passing multiple types', () => {
+        const artistsAndAlbums = search('blackbear', ['artist', 'album']);
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=blackbear&type=artist,album');
+      });
+    });
+
+    it('should return JSON Data from the Promise', () => {
+      promise.resolves({ body: 'json' });
+
+      const artists = search('blackbear', 'artist');
+      expect(artists.resolveValue).to.be.eql({ body: 'json' });
+    });
+  });
 
 });
