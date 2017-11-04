1f5d4d08e438f60417d67976f9571bf78c1f2c6e
Caue Queiroz <cauenqueiroz@gmail.com>
2017-11-04 17:28:42 -0200

    Create another example file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_URL = 'https://api.spotify.com/v1/';
 
-const TOKEN_API = 'BQCp8U4m3wZl6kPq7NFm79zD9cAMRKSlJAd8axzk4Ti_k92ZzACoMI-XISyoJDg4x5e14ISco7WlHEZnq_texgH5r8PXMuI5w6JEsA_kcrIuR_CH3CEN9LrzNsRrZ2luAVaWSLp0-tYxQ97Z';
+const TOKEN_API = 'BQCF3cAGuwe61QqPeoRNYr07Itt7psvQQm2UGfSVdGGy_kG9G8rn1yQhqVxpU4pqr31LnzJprzB8vShutkq-wAf7xY2HrFBzACHcwmLXpujTttsecBp2pwMbK7pMcRfPaP53wD27nZDDLgJ9';
 const HEADERS = {
   headers: {
     Authorization: `Bearer ${TOKEN_API}`,
